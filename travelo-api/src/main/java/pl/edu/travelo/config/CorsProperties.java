f3ded259efef45e21fe02797de1dd6e5c914e5db
Anastasia561 <kurulenkoanastasia@gmail.com>
2026-05-20 16:51:17 +0200

    ref: refactor cors

--- travelo-api/src/main/java/pl/edu/travelo/config/CorsProperties.java
+++ travelo-api/src/main/java/pl/edu/travelo/config/CorsProperties.java
@@ -1,11 +1,11 @@
 package pl.edu.travelo.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
-@Configuration
+@Component
 @ConfigurationProperties(prefix = "cors")
 public class CorsProperties {
     private List<String> allowedOrigins;
